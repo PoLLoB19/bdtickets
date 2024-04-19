9fa7cc5020c946f9ea2e3b77bfb65554ffcbe0f9
PoLLoB19 <pollobatwork19@gmail.com>
2024-04-19 12:52:06 +0600

    sf

--- script.js
+++ script.js
@@ -14,31 +14,41 @@ const Box = document.querySelector(".search_butttons");
 const button = document.querySelectorAll(".search_button");
 const fromBox = document.querySelector(".select");
 const input = document.querySelector(".input");
-
-button.forEach((items) => {
-  items.addEventListener("click", () => {
-    fromBox.innerHTML = items.innerHTML;
-  });
-});
+const push =document.querySelectorAll(".push");
 
 input.onkeyup = function () {
   let result = [];
   let inputit = input.value;
-  if (inputit.length) {
+  if (inputit) {
     result = Data.filter((words) => {
       return words.toLowerCase().includes(inputit.toLowerCase());
     });
   }
 
   function display() {
-   let alone = result.map((items)=>{
-       return `<div>${items}</div>`
-   })
-    
-   alone.map((item)=>{
-      Box.innerHTML = `<button class="search_button">${item}</button>`;
-   })
-  
+    let alone = result.map((items) => {
+      return `<div class='search_button div_b'>${items}</div>`;
+    });
+
+    Box.innerHTML = `<button class="push" >${alone.join("")}</button>`;
+
+
   }
+
   display();
 };
+push.forEach((item)=>{
+item.addEventListener('click',()=>{
+  fromBox.innerHTML= item.innerText;
+  console.log(13)
+})
+})
+button.forEach((items) => {
+  items.addEventListener('click',()=>{
+    fromBox.innerHTML =items.innerText;
+   
+  })
+});
+
+
+
